109e2676cd32633ce78ad14f7e4b606de1d7473a
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-03-04 23:16:22 +0530

    Added: Ability to create a BST logic

---
+++ src/com/bridgelabz/MyBinaryNode.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+public class MyBinaryNode<K extends Comparable<K>> {
+	K key;
+	public MyBinaryNode<K> left;
+	public MyBinaryNode<K> right;
+
+	public MyBinaryNode(K key) {
+		this.key = key;
+		this.left = left;
+		this.right = right;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+}
--- src/com/bridgelabz/MyBinaryTreeMain.java
+++ src/com/bridgelabz/MyBinaryTreeMain.java
@@ -1,8 +1,17 @@
+/*
+ *Ability to create a BST by adding 56 and then adding 30 & 70 
+ *First add 56 as root node so 30 will be added to left and 70 to right
+ */
 package com.bridgelabz;
 
 public class MyBinaryTreeMain {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Binary Search Tree using Java Generics");
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		int size = myBinaryTree.getSize();
+		System.out.println("size = " + size);
 	}
 }
